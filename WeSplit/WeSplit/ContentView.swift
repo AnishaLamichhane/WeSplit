ea6507edfb3c29754948956874ff0bef7e4e5c9a
AnishaLamichhane <52813885+AnishaLamichhane@users.noreply.github.com>
2021-03-27 18:50:36 +0545

    Add files via upload

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Anisha Lamichhane on 3/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = "" //SwiftUI must use strings to store text field values.
+    @State private var numOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [ 5 , 10 , 15 , 20, 25, 0]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount:", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("No of People:", selection: $numOfPeople) {
+                        ForEach( 2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
